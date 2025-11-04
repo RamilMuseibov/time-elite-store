41eed90de617825d7c2cfb246db1d60595660113
RamilMuseibov <Ramilmys@gmail.com>
2025-11-05 01:44:28 +0700

    В процессе редактирования

--- src/App.jsx
+++ src/App.jsx
@@ -503,6 +503,16 @@ function Main() {
           </div>
         </div>
       </section>
+      <section className="online-consultation-section">
+        <div className="online-consultation-container">
+          <h1 className="online-consultation-title">Готовы найти свои идеальные часы?</h1>
+          <p className="online-consultation-sibtitle">Посетите наш шоу-рум или закажите онлайн-консультацию с экспертом</p>
+          <div className="online-consultation-btn-container">
+            <button className="showroom-btn">Посетить шоу-рум</button>
+            <button className="online-consultation-btn">Онлайн-консультация</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
